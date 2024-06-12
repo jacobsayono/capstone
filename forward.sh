0e5de8515fec683bf55598824819a206c134480f
Jacob Sayono <jrsayono@gmail.com>
2024-06-12 04:32:36 -0700

    source inner layer scripts for ros

--- forward.sh
+++ forward.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+source /opt/ros/humble/setup.bash
+
+
 ros2 topic pub /cmd_vel_tracker geometry_msgs/msg/Twist '{linear: {x: 0.6, y: 0, z: 0}, angular: {x: 0, y: 0, z: 0}}' -1
 
 ros2 topic pub /cmd_vel_tracker geometry_msgs/msg/Twist '{linear: {x: 0.6, y: 0, z: 0}, angular: {x: 0, y: 0, z: 0}}' -1
